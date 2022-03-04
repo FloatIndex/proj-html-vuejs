a3136c53eda483c4a8b574bcc352f2213a2f62cc
Marianna Galuppi <marianna.galuppi@gmail.com>
2022-03-04 19:01:23 +0100

    macrostruttura

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer>
+
+        <!-- 
+        <i class="fa-brands fa-instagram"></i>
+        <i class="fa-brands fa-linkedin-in"></i>
+        <i class="fa-brands fa-facebook-f"></i>
+        <i class="fa-brands fa-twitter"></i>
+        -->
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'MyFooter'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
